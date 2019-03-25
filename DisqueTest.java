b060ba13bb35b8fc48c93882094e72568386fe8d
Samyy31 <48915950+Samyy31@users.noreply.github.com>
2019-03-25 17:21:14 +0100

    New update Hanoi

---
+++ DisqueTest.java
@@ -0,0 +1,28 @@
+package cfranc.hanoi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DisqueTest {
+	
+	Disque dSmall = new Disque(1);
+	Disque dMedium = new Disque(2);
+	Disque dTall = new Disque(3);
+
+	@Test
+	public void testCompareTo() {
+		Disque petit = new Disque(1);
+		Disque moyen = new Disque(2);
+		Disque grand = new Disque(3);
+		assertEquals(-1, petit.compareTo(moyen));
+		assertEquals(-1, moyen.compareTo(grand));
+	}
+
+	@Test
+	public void compareTo_SmallMedium_Negative(){
+		int expected = -1;
+		int actual = dSmall.compareTo(dMedium);
+		assertEquals(expected, actual);
+	}
+}
